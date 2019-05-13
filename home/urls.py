e3cae24bcb5f1315f214baaae230ed2280e70629
Kyle Chatman <kchatman@gmail.com>
2019-05-12 21:40:42 -0700

    use github pages for home

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from home import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("self/", views.self, name="self"),
-    path("SE/", views.SE, name="SE"),
-    path("CS/", views.CS, name="CS"),
+    path("", RedirectView.as_view(url='https://etoitau.github.io/home/index.html'), name="index"),
+    path("self/", RedirectView.as_view(url='https://etoitau.github.io/home/self.html'), name="self"),
+    path("SE/", RedirectView.as_view(url='https://etoitau.github.io/home/SE.html'), name="SE"),
+    path("CS/", RedirectView.as_view(url='https://etoitau.github.io/home/CS.html'), name="CS"),
 ]
